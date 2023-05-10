b878039a538ca21bf96c0e23ba1f8d3707120f64
muddassarH <muddassarhussain90@gmail.com>
2023-05-10 15:21:38 +0500

    day 27 : send api news through email

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 
+from send_api_to_email import send_email
+
 api_key = "fcbbd2d228124532a6aa0b367d17cf2d"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-04-09&sortBy=publishedAt&" \
       "apiKey=fcbbd2d228124532a6aa0b367d17cf2d"
@@ -9,5 +11,10 @@ req = requests.get(url)
 # we get a dict
 content = req.json()
 # access the article titles ans decription
+message =""
 for article in content['articles']:
-    print(article['title'])
+    if article['title'] is not None:
+        message = message + article['title'] + "\n" + article['description'] + 2*"\n"
+
+message = message.encode("utf-8")
+send_email(message)
---
+++ send_api_to_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "muddassarhussain90@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver_email = "muddassarhussain90@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver_email, message)
